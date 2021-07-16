bcb2e10de566f59f6ec063db401f0c0acea647f5
rsanlazaro <rsanlazaro@hotmail.com>
2021-07-15 20:58:35 -0500

    [ADD] Estructura de CRUD

--- website/routes/products.js
+++ website/routes/products.js
@@ -5,6 +5,21 @@ const router = express.Router();
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
+/*** GET ALL PRODUCTS ***/
 router.get('/', productsController.index);
+
+/*** CREATE ONE PRODUCT ***/
+router.get('/create/', productsController.create);
+// router.post('/', uploadFile.single('productImage'), productsController.store);
+
+/*** GET ONE PRODUCT ***/
+router.get('/:id/', productsController.detail);
+
+/*** EDIT ONE PRODUCT ***/
+router.get('/edit/:id', productsController.edit);
+// router.put('/:id', uploadFile.single('productImageEdit'), productsController.update);
+
+/*** DELETE ONE PRODUCT***/
+// router.delete('/:id', productsController.destroy);
 
 module.exports = router;
